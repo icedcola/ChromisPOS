514b2a11bdfbca8a4c5f2ba5ad03ced6f6d518c4
Matt Henderson <heenie@gmail.com>
2016-01-01 12:59:18 +1030

    Added XML formatting import for screen and cleaned up code a bit.

--- src-pos/uk/chromis/pos/customerscreen/CustomerScreen.java
+++ src-pos/uk/chromis/pos/customerscreen/CustomerScreen.java
@@ -5,11 +5,23 @@
  */
 package uk.chromis.pos.customerscreen;
 
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
-import javax.swing.table.TableModel;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 import uk.chromis.pos.ticket.TicketInfo;
 import uk.chromis.pos.ticket.TicketLineInfo;
 
@@ -22,13 +34,16 @@ public class CustomerScreen {
     private static JFrame mainFrame = new JFrame();
     private static JTable ticketTable;
     private static Object[][] tableData = new Object[1][2];
-    private static final String columnNames[] = {"Item", "Price"};
-    private static int itemCount = 0;
+    private static final String COLUMN_NAMES[] = {"Item", "Price"};
     
     public CustomerScreen() {
-        System.out.println("Woot");
-        ticketTable = new JTable(tableData, columnNames);
-        mainFrame.add(ticketTable);
+        
+        
+        ticketTable = new JTable(tableData, COLUMN_NAMES);
+        
+        parseXMLStyle("custscreenimages/layout.xml");
+        
+        mainFrame.add(ticketTable, BorderLayout.CENTER);
         mainFrame.pack();
         mainFrame.setVisible(true);
     }
@@ -46,23 +61,59 @@ public class CustomerScreen {
             tableData[ticketline.getTicketLine()][1] = "$".concat(String.valueOf(ticketline.getValue()));
         }
         
-        ticketTable.setModel(new DefaultTableModel(tableData, columnNames));
+        ticketTable.setModel(new DefaultTableModel(tableData, COLUMN_NAMES));
     }
     
-    public static void addLine(String itemName, String itemPrice) {
-        tableData[itemCount][0] = itemName;
-        tableData[itemCount][1] = itemPrice;
-        itemCount += 1;
-        System.out.println("add line...");
-        System.out.println(itemName);
-        System.out.println(itemPrice);
-      
+    public static void parseXMLStyle(String filename) {
+        Document layoutxml;
+        SAXBuilder builder = new SAXBuilder();
+        File xmlFile = new File(filename);
+        
+        
+             
+        try {
+            layoutxml = builder.build(xmlFile);
+            
+            //Set table properties
+            BorderLayout layout = new BorderLayout();
+
+            layout.setHgap(Integer.parseInt(layoutxml.getRootElement().getAttributeValue("border", "5")));
+            layout.setVgap(Integer.parseInt(layoutxml.getRootElement().getAttributeValue("border", "5")));
+            
+            ticketTable.setFont(ticketTable.getFont().deriveFont(Integer.parseInt(layoutxml.getRootElement().getAttributeValue("fontsize", "16"))));
+             
+            mainFrame.setLayout(layout);
+
+            for (Object o : layoutxml.getRootElement().getChildren()) {
+                Element e = (Element) o;
+                if ("imagefile".equals(e.getName())) {
+                    BufferedImage image = ImageIO.read(new File(e.getText()));
+
+                    //Scale image, if the user doesn't give either width or height
+                    //then -1 is input, which keeps the ratio.  If both are -1, then original
+                    //size is used.
+                    JLabel imageLabel = new JLabel(new ImageIcon(image.getScaledInstance(
+                            Integer.parseInt(e.getAttributeValue("width", "-1")),
+                            Integer.parseInt(e.getAttributeValue("height", "-1")),
+                            Image.SCALE_SMOOTH)));
+                    mainFrame.add(imageLabel, e.getAttributeValue("position", BorderLayout.PAGE_START));
+                }
+            }
+
+        } catch (JDOMException ex) {
+            Logger.getLogger(CustomerScreen.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(CustomerScreen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
     }
-    
-    public static void clearLines() {
-        tableData = new Object[10][2];
-        itemCount = 0;
-        System.out.println("clear lines...");
+
+    private static Object getValueOrDefault(Object value, Object defaultValue) {
+        System.out.println(value);
+        System.out.println(defaultValue);
+        return (value != null) ? value : defaultValue;
     }
-    
 }
